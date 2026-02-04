128a813a65c5cb488ed53386c41f5e2795fad603
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-02-04 07:16:31 +0500

    the and

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+  import React, { useState } from 'react'
+  import datas from './data/data.js'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  function App() {
+    const [data, setData] = useState(datas)
 
-export default App
+    function likes (id){
+      console.log(id);
+      let arr = data.map((item) => {
+        if(item.id === id){
+          return {...item, likes: item.likes + 1}
+        }else{
+         return item
+        }
+      })
+      setData(arr)
+    }
+    function disLikes (id){
+      console.log(id);
+      let arr = data.map((item) => {
+        if(item.id === id){
+          return {...item, disLikes: item.disLikes + 1}
+        }else{
+         return item
+        }
+      })
+      setData(arr)
+    }
+    function deletes (id){
+      console.log(id);
+      let arr = data.filter((item) => {
+        return item.id !== id
+      })
+      setData(arr)
+    }
+    return (
+      <div className='images-container'>
+        {data.map((item) => {
+          return (
+              <div className='buttons-container'>
+                <button onClick={() => likes(item.id)}>+</button>
+                <button onClick={() => disLikes(item.id)}>-</button>
+                <button onClick={() => deletes(item.id)}>🗑</button>
+              </div>
+          );
+        })}
+      </div>
+    )
+  }
+
+  export default App
